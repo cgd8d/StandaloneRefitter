3e11277e3ce66c8a910c5cb46dab06d8f3ad91f4
clayton <clayton@AOD270.home>
2013-09-12 22:10:15 -0400

    Add build.sh to the repository -- only functional at NERSC for now.

--- build.sh
+++ build.sh
@@ -2,12 +2,17 @@
 # Note: exo-config --incflags seems to not work properly.  But if you can get it working, more power to you!
 # I test explicitly for preprocessor macros which are needed.
 
-# For local builds without BLAS, just to get things working.
+# At NERSC -- will require adaptation for SLAC, of course.
 g++ -g -O3 \
 -I`exo-config --incdir` `root-config --cflags` \
 $(if [[ "$(exo-config --incflags)" == *HAVE_TYPE_TRAITS=1* ]]; then echo "-DHAVE_TYPE_TRAITS=1"; fi) \
 `exo-config --libflags` `root-config --libs` \
 -I$MKL_INC $MKL_LP64 -Wl,-rpath,$MKL_LIBDIR \
 -o Refitter \
-*.cc
-
+EXORefitSignals.cc Refitter.cc
+g++ -g -O3 \
+-I`exo-config --incdir` `root-config --cflags` \
+$(if [[ "$(exo-config --incflags)" == *HAVE_TYPE_TRAITS=1* ]]; then echo "-DHAVE_TYPE_TRAITS=1"; fi) \
+`exo-config --libflags` `root-config --libs` \
+-o ConvertNoiseFile \
+ConvertNoiseFile.cc
