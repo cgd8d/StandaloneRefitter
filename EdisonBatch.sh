670837f794ab71379fca1879de42c869cc470592
clayton <clayton@AOD270.home>
2013-09-27 11:38:17 -0400

    Update edison submit script to call python MPI wrapper.

--- EdisonBatch.sh
+++ EdisonBatch.sh
@@ -8,6 +8,5 @@
 export CRAY_ROOTFS=DSL
 cd $SCRATCH
 cp $HOME/edison/StandaloneRefitter/Refitter .
-aprun -n 1 -d 12 -ss -cc 0-11 ./Refitter proc00004544-000.root run00004544-000.root Denoised0.root 0 1000 : \
-      -n 1 -d 12 -ss -cc 12-23 ./Refitter proc00004544-000.root run00004544-000.root Denoised1.root 1000 1000
+aprun -n 2 -S 1 -d 12 -ss -cc numa_node $PBS_O_WORKDIR/MPIWrapper.py ./Refitter $SCRATCH/InOutFiles
 
