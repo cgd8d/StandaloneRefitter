a6a3be1efdc3f828f430e29d9ecefae04444574c
clayton <clayton@AOD270.home>
2013-10-01 01:35:11 -0400

    Make the edison build script just a little more static, by linking statically to EXOAnalysis and a few other libraries pulled in by EXOAnalysis.  Should be straightforward to adapt to hopper; not sure if it's worth it at SLAC.

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ if [ -z "$NERSC_HOST" ]; then
 	# Running at SLAC.
 	export CXX=g++
 	export LOCATION=SLAC
+	export EXO_LIBS="-lEXOAnalysisManager -lEXOCalibUtilities -lEXOUtilities"
 else
 	# Running at NERSC
 	export CRAY_ROOTFS=DSL
@@ -21,10 +22,12 @@ else
 		export CXX='/opt/gcc/4.7.2/snos/bin/g++ -std=c++11'
 		# define USE_LOCKFREE when it is made available -- ticked filed.
 		export THREAD_MACROS="-DUSE_THREADS -DNUM_THREADS=6"
+		export EXO_LIBS="-lEXOAnalysisManager -lEXOCalibUtilities -lEXOUtilities"
 	elif [ "$NERSC_HOST" = "edison" ]; then
 		export LOCATION=EDISON
 		export CXX='/opt/gcc/4.8.1/snos/bin/g++ -std=c++11'
 		export THREAD_MACROS="-DUSE_THREADS -DNUM_THREADS=12 -DUSE_LOCKFREE"
+		export EXO_LIBS="-Wl,-Bstatic -Wl,-u,gEXOCalibBuilder_EXOElectronicsShapersHandler -Wl,-u,gEXOCalibBuilder_EXOUWireGainsHandler -Wl,-u,gEXOCalibBuilder_EXOChannelMapHandler -lEXOAnalysis -L$FFTW_DIR -L/usr/common/usg/mysql/5.1.63/lib/mysql -lfftw3 -lmysqlclient -Wl,-Bdynamic"
 	else
 		echo "No match to nersc host."
 		exit 1
@@ -34,11 +37,13 @@ fi
 # Should work on Hopper, Edison, or at SLAC.
 $APRUN $CXX -O3 -pthread \
 `root-config --cflags` -I`exo-config --incdir` -DHAVE_TYPE_TRAITS=1 -D$LOCATION $THREAD_MACROS \
--L`root-config --libdir` -lRIO -lHist -lGraf -lTree \
--L`exo-config --libdir` -lEXOAnalysisManager -lEXOCalibUtilities -lEXOUtilities \
+-L`root-config --libdir` \
+-L`exo-config --libdir` \
 -I$MKL_INC -L$MKL_LIBDIR \
 -o Refitter \
 Refitter.cc EXORefitSignals.cc SafeStopwatch.cc \
+$EXO_LIBS \
+-lRIO -lHist -lGraf -lTree -lNet -lXMLParser -lGpad -lTreePlayer \
 -Wl,-Bstatic \
 $BOOST_FLAGS \
 -Wl,--start-group -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -Wl,--end-group \
