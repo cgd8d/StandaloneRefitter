8c5caf5d98e2f313ab3b51ee78013c8d2c8c93e9
clayton <clayton@AOD270.home>
2013-10-18 02:58:34 -0400

    Update the EdisonBatch script to be similar to the HopperBatch script, in particular to be compatible with using xrootd.

--- EdisonBatch.sh
+++ EdisonBatch.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 #PBS -q regular
 #PBS -l mppwidth=24
-#PBS -l walltime=00:40:00
+#PBS -l walltime=04:00:00
 #PBS -N Refitter
 #PBS -j oe
 
 export CRAY_ROOTFS=DSL
 cd $SCRATCH
-cp $HOME/edison/Refitter/Refitter .
-aprun -n 1 -S 1 -d 12 -ss -cc numa_node ./Refitter $SCRATCH/InOutFiles/infile0000.txt
 
+echo "For a check, are there any already-running instances of socat?"
+ps -Af | grep socat
+
+SOCAT_PORT=`$PBS_O_WORKDIR/GetSocatPort.sh`
+/project/projectdirs/exo200/exo_out/bin/socat TCP4-LISTEN:$SOCAT_PORT,fork TCP4:scalnx-v02.slac.stanford.edu:3967 &
+socatpid=$!
+echo "We will communicate through $HOST:$SOCAT_PORT; socat has pid $socatpid."
+
+aprun -n 2 -S 1 -d 12 -ss -cc numa_node $PBS_O_WORKDIR/Refitter $SCRATCH/LightOnly/InOutFiles $HOST:$SOCAT_PORT &
+pid=$!
+
+trap "echo 'user requested termination'; kill -USR1 $pid" USR1
+trap "echo 'term'" TERM
+wait $pid
+kill $socatpid
+wait $socatpid
+echo "Killed socat."
