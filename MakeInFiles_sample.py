8d3fc1710409479729cdc42535909316d60ae857
clayton <clayton@AOD270.home>
2013-10-25 12:16:10 -0400

    Fixes to MakeInFiles_sample.py.

--- MakeInFiles_sample.py
+++ MakeInFiles_sample.py
@@ -7,8 +7,13 @@ ToDO:
 Extract the number of entries in the processed run file from metadata.
 Verify that the processed file I see is truly available -- don't want to crash later for missing file.
 """
+from __future__ import with_statement
+import os
+import glob
+import ROOT
+ROOT.gSystem.Load("libEXOUtilities")
 
-ProcsPerJob = 10
+ProcsPerJob = 100
 DenoisedOutDir = "/scratch1/scratchdirs/claytond/LightOnly"
 
 NoiseFileBase = "/global/u1/c/claytond/NoiseCorrFiles"
@@ -18,27 +23,25 @@ def GetNoiseFile(runNo):
     noiseFile = ""
     for noiseWindow in RunWindows:
         if runNo >= noiseWindow[0] and runNo <= noiseWindow[1]:
-            return = NoiseFileBase + "/%i_to_%i.dat" % noiseWindow
+            return NoiseFileBase + "/%i_to_%i.dat" % noiseWindow
     # Currently I refuse to pick a noise file by any heuristic here.
     raise ValueError("No noise file for run %i." % runNo)
 
-import ROOT
-ROOT.gSystem.Load("libEXOUtilities")
 ProcDataset = ROOT.EXORunInfoManager.GetDataSet("Data/Processed/masked",
                                                 "run>=2464&&run<=5367")
 
 def JobForProc(procFile, runNo):
-    FileParts = procFile.split('.')[-6:]
+    FileParts = procFile.split('/')[-6:]
     FileBase = FileParts[-1][-17:]
     RawFileParts = FileParts
     RawFileParts[3] = 'root'
     RawFileParts[-1] = 'run' + FileBase
-    return "%s\n%s\n%s\n%s\n%i\n%i\n%f\n" %
-           ('/'.join(FileParts), # Processed file
-            '/'.join(RawFileParts), # Raw file
-            "%s/%i/denoised%s" % (DenoisedOutDir, runNo, FileBase), # Out file
-            GetNoiseFile(runNo), # noise file
-            0, -1, 0.1) # Run parameters
+    return ("%s\n%s\n%s\n%s\n%i\n%i\n%f\n" %
+            ('/' + '/'.join(FileParts), # Processed file
+             '/' + '/'.join(RawFileParts), # Raw file
+             "%s/%i/denoised%s" % (DenoisedOutDir, runNo, FileBase), # Out file
+             GetNoiseFile(runNo), # noise file
+             0, -1, 0.1)) # Run parameters
 
 OutRunList = []
 ProcList = []
@@ -47,12 +50,12 @@ for runInfo in ProcDataset:
         ProcsToInsert = []
         for runFile in runInfo.GetRunFiles():
             Job = JobForProc(runFile.GetFileLocation(), runInfo.GetRunNumber())
-            numEntries = runFile.FindMetaData("nEntries").AsInt()
+            numEntries = runFile.FindMetaData("eventCount").AsInt()
             ProcsToInsert.append((numEntries, Job))
         ProcList += ProcsToInsert
         print "Added jobs for run %i." % runInfo.GetRunNumber()
-    except:
-        print "Failed to add jobs for run %i." % runInfo.GetRunNumber()
+    except ValueError, exc:
+        print "Failed to add jobs for run %i: %s." % (runInfo.GetRunNumber(), exc.message)
 
 # Try to group processes of similar length together, so they finish in similar times.
 # Also make the shortest jobs go into the smallest group, since this should also reduce waste.
@@ -69,4 +72,5 @@ while len(ProcList) > 0:
     for i in xrange(len(ProcsInThisJob)):
         with open('Job%04i/infile%04i.txt' % (JobIndex, i), 'w') as infile:
             infile.write(ProcsInThisJob[i])
+    JobIndex += 1
 
