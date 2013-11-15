40b115ec5a422c4d19abedda87a4af8360c8abc9
clayton <clayton@AOD270.home>
2013-11-15 00:11:13 -0500

    Add a short batch submission script for SLAC, to be run with "bsub < SLACBatch.sh".

---
+++ SLACBatch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#BSUB -n 16
+#BSUB -q bulletmpi
+#BSUB -R span[hosts=1]
+#BSUB -o LogFiles/Job0000.out
+#BSUB -e LogFiles/Job0000.err
+mpirun -np 2 ./Refitter /nfs/slac/g/exo_data4/users/cgd8d/rhel6-64/JobFiles/Job0000 exo-rdr.slac.stanford.edu
+
