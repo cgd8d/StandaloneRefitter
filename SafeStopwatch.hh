2b2a962727c43e24b64b033c7ff7d17184e6f25a
clayton <clayton@AOD270.home>
2013-12-18 12:05:12 -0500

    Make SafeStopwatch a header-only class.  Also, make it thread-safe even if USE_THREADS is undefined; this simplifies code and shouldn't hurt in any case.

--- SafeStopwatch.hh
+++ SafeStopwatch.hh
@@ -1,8 +1,7 @@
 /*
 Create a thread-safe stopwatch -- multiple threads can use this watch concurrently without any sort of locking.
 The resulting time will be the sum of the times of all of the component stopwatches.
-If USE_THREADS is undefined, this stopwatch will use POSIX calls instead of BOOST,
-and no thread-safety properties are guaranteed.
+Even if USE_THREADS is undefined, we'll still use thread-safe calls, with some small timing penalty.
 
 Proper use:
 void f() {
@@ -19,35 +18,55 @@ thread-safe in gcc for a long time.  Therefore, it is fine for our purposes.
 #ifndef SafeStopwatch_hh
 #define SafeStopwatch_hh
 
-#ifdef USE_THREADS
 #include <boost/atomic.hpp>
 #include <boost/timer/timer.hpp>
-#else
-#include <time.h>
-#endif
 #include <iostream>
 #include <string>
+#include <cassert>
 
 class SafeStopwatch
 {
  public:
-#ifdef USE_THREADS
   typedef boost::timer::cpu_timer tag;
-#else
-  typedef timespec tag;
-#endif
-  SafeStopwatch(std::string id);
-  ~SafeStopwatch();
-  tag Start() const;
-  void Stop(tag& t);
-  void Print() const;
+
+  SafeStopwatch(std::string id) {
+    // Constructor -- *NOT* guaranteed thread-safe.
+    // (You must finish constructing a stopwatch before anyone uses it.)
+    assert(fNanoseconds.is_lock_free());
+    fNanoseconds.store(0, boost::memory_order_relaxed);
+    fID = id;
+  }
+
+  ~SafeStopwatch() {Print();}
+
+  tag Start() const {
+    // Get a "tag", which identifies the start time.
+    // This is what will let the class compute a duration when "stop" is later called.
+    // This function is thread-safe.
+    return tag();
+  }
+
+  void Stop(tag& t) {
+    // Increment the stopwatch time by the duration between when tag was created and now.
+    // This function is thread-safe.
+    t.stop();
+    underlying_rep duration = t.elapsed().wall;
+    fNanoseconds.fetch_add(duration, boost::memory_order_relaxed);
+  }
+
+  void Print() const {
+    // Print the time of the stopwatch, terminated by an endline.
+    // Any timers which have not been stopped will have no effect on the time printed.
+    // Printing from multiple threads may yield garbage,
+    // but technically this function is thread-safe.
+    std::cout<<fID<<" has accumulated ";
+    std::cout<<double(fNanoseconds.load(boost::memory_order_relaxed))/1e9;
+    std::cout<<" sec."<<std::endl;
+  }
+
  private:
-#ifdef USE_THREADS
   typedef boost::timer::nanosecond_type underlying_rep;
   typedef boost::atomic<underlying_rep> rep;
-#else
-  typedef long long int rep;
-#endif
   rep fNanoseconds;
   std::string fID;
 };
